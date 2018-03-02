de62b7b19463bdf4d748eec96b4c31496ac92a18
Gustavo Riposati <guriposa@gmail.com>
2018-03-02 20:26:46 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,29 +1,85 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "headers/cabecalho_funcoes_vector_meu.h"
+#include<string.h>
+
+char *vet;
+int tamArrayInicial = 10;
+int valor;
+
+void alocaMemoria(){
+
+    vet = (char *)malloc(tamArrayInicial * sizeof(char));
+    memset(vet,0,sizeof(char) * tamArrayInicial);
+}
+
+void dobraBuffer(){
+
+    int i;
+    char *t = vet;
+
+    char u[tamArrayInicial];
+
+    for(i=0;i<tamArrayInicial;i++){
+        u[i] = t[i];
+    }
+
+    char *p = vet;
+    p = (char *)realloc(p,(tamArrayInicial*2) * sizeof(char));
+
+    memset(p,0,sizeof (char) * (tamArrayInicial*2));
+
+    for(i=0;i<tamArrayInicial;i++){
+        p[i] = u[i];
+    }
+    tamArrayInicial = tamArrayInicial * 2;
+    vet = p;
+}
+
+void mostraBuffer(){
+
+    int k;
+    printf("valores alocados\n");
+    for(k=0;k<strlen(vet);k++){
+
+        printf("%c",vet[k]);
+    }
+}
+
+void necessarioDobrar(){
+
+    if(vet[tamArrayInicial-1]!=0){ /// sabe que precisa alocar mais memoria
+        printf("\n\n*** dobrei o tamanho do vetor *** valor nesse momento = %d\n\n",valor);
+        dobraBuffer();
+    }
+}
 
 int main()
 {
-    int tamArrayInicial = tamanhoBufferDesejado(); /// mesmo tamanho que o buffer tá alocando
-    int *vet = alocaMemoria(); /// vetor normal
-    int valor=0; /// essa variavel analisa se precisa alocar mais RAM
-    int k;
+    alocaMemoria(); /// vetor normal
     char opcao;
 
     printf("\nDigite e tecle enter\n<< diferente de zero - inserir valores >>\n<< 0 - sair >>\n");
     scanf(" %c",&opcao);
 
+    getchar();
+
+    char *frase = "Teste String em C Gustavo Riposati estou aqui testando essa minha implementacao ";
+
     while(opcao!='0'){
 
-        printf("digite um valor e tecle enter pra alocar\n");
-        scanf("%d",&vet[valor]);
-        valor++;
+        int i=0;
+        while(frase[i]!='\0'){
+            vet[valor] = frase[i];
+            valor++;
+            i++;
 
-        tamArrayInicial = necessarioDobrar(vet,valor,tamArrayInicial);
+            necessarioDobrar();
+        }
 
-        mostraBuffer(vet,tamArrayInicial);
+        mostraBuffer();
         printf("\nDigite e tecle enter\n<< diferente de zero - inserir valores >>\n<< 0 - sair >>\n");
         scanf(" %c",&opcao);
+        getchar();
     }
         free(vet);
         return 0;
